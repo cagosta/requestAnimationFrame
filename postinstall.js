2b836418de615e3ae3c71999010f623ec0228c7d
cyril agosta <cyril.agosta@gmail.com>
2014-01-07 15:13:56 +0100

    cleaning for travis

--- postinstall.js
+++ postinstall.js
@@ -4,10 +4,10 @@
 
 
 var bower = require( 'bower' )
-console.log('Installing bower dependencies ... ')
+console.log( 'Installing bower dependencies ... ' )
 
-bower.commands.install([]).on('end', function( ){
-    console.log('args', arguments)
-    console.log('Installed bower dependencies.')
+bower.commands.install( [] ).on( 'end', function() {
     
-})
+    console.log( 'Installed bower dependencies.' )
+
+} )
